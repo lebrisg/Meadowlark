1aaffd3150584b735683454e8252323d3dce1abf
Gilles Le Bris <lebrisg@hotmail.com>
2018-03-12 12:27:27 +0100

    Update meadowlark.js

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,16 @@ var app = express();
 
 app.set('port', process.env.PORT || 8080);
 
+app.get('/', function(req, res) {
+	res.type('text/plain');
+	res.send('Meadowlark Travel');
+});
+
+app.get('/about', function(req, res) {
+	res.type('text/plain');
+	res.send('About Meadowlark Travel');
+});
+
 app.use(function(req, res) {
 	res.type('text/plain');
 	res.status(404);
